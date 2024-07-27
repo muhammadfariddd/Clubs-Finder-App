c0547a2444f248949f320eab07d4f575464ea562
muhammadfariddd <muhammadfariddd2004@gmail.com>
2024-07-28 06:05:56 +0700

    first commit

---
+++ src/script/components/index.js
@@ -0,0 +1,4 @@
+import "./app-bar.js";
+import "./footer-bar.js";
+import "./club-item.js";
+import "./club-list.js";
---
+++ src/script/view/home.js
@@ -0,0 +1,62 @@
+import Utils from "../utils.js";
+import Clubs from "../data/local/clubs.js";
+
+const home = () => {
+  const searchFormElement = document.querySelector("#searchForm");
+
+  const clubListContainerElement = document.querySelector("#clubListContainer");
+  const clubQueryWaitingElement =
+    clubListContainerElement.querySelector(".query-waiting");
+  const clubLoadingElement =
+    clubListContainerElement.querySelector(".search-loading");
+  // const clubListElement = clubListContainerElement.querySelector(".club-list");
+  const clubListElement = clubListContainerElement.querySelector("club-list");
+  const listElement = clubListElement.querySelector(".list");
+
+  const showSportClub = (query) => {
+    showLoading();
+
+    const result = Clubs.searchClub(query);
+    displayResult(result);
+
+    showClubList();
+  };
+
+  const onSearchHandler = (event) => {
+    event.preventDefault();
+
+    const query = event.target.elements.name.value;
+    showSportClub(query);
+  };
+
+  const displayResult = (clubs) => {
+    const clubItemElements = clubs.map((club) => {
+      const clubItemElement = document.createElement("club-item");
+      clubItemElement.club = club;
+
+      return clubItemElement;
+    });
+
+    Utils.emptyElement(clubListElement);
+    clubListElement.append(...clubItemElements);
+  };
+
+  const showLoading = () => {
+    Array.from(clubListContainerElement.children).forEach((element) => {
+      Utils.hideElement(element);
+    });
+    Utils.showElement(clubLoadingElement);
+  };
+
+  const showQueryWaiting = () => {
+    Array.from(clubListContainerElement.children).forEach((element) => {
+      Utils.hideElement(element);
+    });
+    Utils.showElement(clubQueryWaitingElement);
+  };
+
+  searchFormElement.addEventListener("submit", onSearchHandler);
+  showQueryWaiting();
+};
+
+export default home;
